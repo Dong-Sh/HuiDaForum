64ba2c635d4f954f72c21cb9343a4f1582b20b19
a784434650 <784434650@qq.com>
2017-09-16 09:03:29 +0800

    zhang

--- app/src/main/java/com/huidaforum/fragment/NewsFragment.java
+++ app/src/main/java/com/huidaforum/fragment/NewsFragment.java
@@ -1,9 +1,16 @@
 package com.huidaforum.fragment;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.huidaforum.R;
+import com.huidaforum.activity.CommentActivity;
+import com.huidaforum.activity.ReplyActivity;
+import com.huidaforum.activity.RewardActivity;
+import com.huidaforum.activity.SystemActivity;
 import com.huidaforum.base.BaseFragment;
 
 /**
@@ -11,9 +18,19 @@ import com.huidaforum.base.BaseFragment;
  */
 
 public class NewsFragment extends BaseFragment {
+
+    private RelativeLayout ll_news_huifu;
+    private RelativeLayout ll_news_dashang;
+    private RelativeLayout ll_news_xitong;
+    private RelativeLayout ll_news_pinglun;
+
     @Override
     public View initView() {
         View view = LayoutInflater.from(mActivity).inflate(R.layout.fragment_news, null);
+        ll_news_huifu = (RelativeLayout) view.findViewById(R.id.ll_news_huifu);
+        ll_news_dashang = (RelativeLayout) view.findViewById(R.id.ll_news_dashang);
+        ll_news_xitong = (RelativeLayout) view.findViewById(R.id.ll_news_xitong);
+        ll_news_pinglun = (RelativeLayout) view.findViewById(R.id.ll_news_pinglun);
         return view;
     }
 
@@ -24,6 +41,33 @@ public class NewsFragment extends BaseFragment {
 
     @Override
     protected void initListener() {
-
+        ll_news_huifu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mActivity, ReplyActivity.class);
+                startActivity(intent);
+            }
+        });
+        ll_news_dashang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mActivity, RewardActivity.class);
+                startActivity(intent);
+            }
+        });
+        ll_news_xitong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mActivity, SystemActivity.class);
+                startActivity(intent);
+            }
+        });
+        ll_news_pinglun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mActivity, CommentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
