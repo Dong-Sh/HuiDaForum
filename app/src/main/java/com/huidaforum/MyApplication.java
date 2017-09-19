ba6ed49b676b3b8fda4737c3014ae8b618408202
zhaogui <954923610@qq.com>
2017-09-19 12:51:31 +0800

    55

---
+++ app/src/main/java/com/huidaforum/MyApplication.java
@@ -0,0 +1,42 @@
+package com.huidaforum;
+
+import android.app.Application;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.cookie.CookieJarImpl;
+import com.lzy.okgo.cookie.store.SPCookieStore;
+import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
+
+import java.util.logging.Level;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by gui on 2017/9/19.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initOkGo();
+    }
+    private void initOkGo() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //配置log打印
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkGo");
+        //log打印级别，决定了log显示的详细程度
+        loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.BODY);
+        //log颜色级别，决定了log在控制台显示的颜色
+        loggingInterceptor.setColorLevel(Level.INFO);
+        builder.addInterceptor(loggingInterceptor);
+        //配置cookie  配置到sp中
+        builder.cookieJar(new CookieJarImpl(new SPCookieStore(this)));
+        OkGo.getInstance().init(this).setOkHttpClient(builder.build())
+                .setCacheMode(CacheMode.REQUEST_FAILED_READ_CACHE)
+                .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)
+                .setRetryCount(3);
+    }
+}
