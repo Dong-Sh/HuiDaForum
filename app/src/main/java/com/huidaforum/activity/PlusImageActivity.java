b2de8b6f1046fd11b8dc20c82a72d7b6250a47ef
zhaogui <954923610@qq.com>
2017-10-16 19:39:07 +0800

    55

---
+++ app/src/main/java/com/huidaforum/activity/PlusImageActivity.java
@@ -0,0 +1,36 @@
+package com.huidaforum.activity;
+
+import android.view.View;
+
+import com.huidaforum.base.BaseActivity;
+
+/**
+ * Created by gui on 2017/10/16.
+ */
+
+public class PlusImageActivity extends BaseActivity {
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void processClick(View v) {
+
+    }
+}
